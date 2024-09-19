5280efe41be4dfdeb8cf9ada68b8f341ec92a0e8
ecopotta <ecopotta707@gmail.com>
2024-09-18 23:38:21 -0300

    Insertado AppContextProvider y BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import {BrowserRouter} from "react-router-dom"
+import { AppContextProvider } from './context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
