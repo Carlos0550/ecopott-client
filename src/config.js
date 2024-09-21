605f8ca2a71e8bdcc691e1414f740ab7ed7af62b
Carlos0550 <carlospelinski03@gmail.com>
2024-09-21 00:21:25 -0300

    .

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const config = {
-    apiBaseUrl:
-    process.env.NODE_ENV === "production"
-    ? "https://ecopott-server.vercel.app"
-    : "http://localhost:4000"
+    apiBaseUrl: "https://ecopott-server.vercel.app"
+    // process.env.NODE_ENV === "production"
+    // ? "https://ecopott-server.vercel.app"
+    // : "http://localhost:4000"
 }
