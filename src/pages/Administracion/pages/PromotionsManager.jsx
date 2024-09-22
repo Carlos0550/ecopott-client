5716c3ada75270dfb21f5c9d5b90f99cd924706c
Carlos0550 <carlospelinski03@gmail.com>
2024-09-22 20:30:22 -0300

    se agrego logica para añadir, editar y eliminar promociones

--- src/pages/Administracion/pages/PromotionsManager.jsx
+++ src/pages/Administracion/pages/PromotionsManager.jsx
@@ -1,27 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import AdminNavbar from "../Navbar/AdminNavbar";
-import { Col, Row, Form, Checkbox, Button, Card, Input, ConfigProvider, DatePicker, notification } from "antd";
+import { Col, Row, Form, Checkbox, Button, Card, Input, ConfigProvider, DatePicker, notification, message, Table, Popconfirm } from "antd";
 import { useAppContext } from "../../../context";
+import { scroller } from "react-scroll"
 import dayjs from 'dayjs';
+
+import Edit from "../../../SVGs/Edit" 
+import DeleteIcon from "../../../SVGs/DeleteIcon"
+import { processPromotions } from "../../../utils/processPromotions";
 
 function PromotionsManager() {
   const [form] = Form.useForm();
   const [api, contextHolder] = notification.useNotification();
 
-  const { products, locale,create_promotion } = useAppContext();
+  const { products, locale,create_promotion,promotions,update_promotion,delete_promotion } = useAppContext();
   const [selectedProducts, setSelectedProducts] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
   const [promoValue, setPromoValue] = useState(0);
   const [startDate, setStartDate] = useState(null);
   const [expDate, setExpDate] = useState(null);
+  const [loading, setLoading] = useState(false)
+  const [deleting, setDeleting] = useState(false)
 
-  const handleFinish = (values) => {
+  const [editingPromotion, setEditingPromotion] = useState(false)
+  const [selectedPromotion, setSelectedPromotion] = useState(null)
+
+  const formRef = useRef(null);
+
+
+  const handleFinish = async(values) => {
     const formData = new FormData()
     if (!startDate || !expDate) {
       openNotification()
       return;
     }
-    console.log(values)
+    setLoading(true)
     formData.append("productsIDs", JSON.stringify(values.groupProducts))
     formData.append("promoName", values.promoName)
     formData.append("promoPrice", values.promoPrice)
@@ -32,11 +45,19 @@ function PromotionsManager() {
     }else{
       formData.append("enabled", false)
     }
+    if (editingPromotion) {
+      formData.append("promotionID", selectedPromotion.id_promotion)
+    }
     // formData.forEach((value, key) => {
     //   console.log(`${key}: ${value}`);
     // });
     
-    create_promotion(formData)
+    editingPromotion ? await update_promotion(formData, selectedPromotion.id_promotion) : await create_promotion(formData)
+    setLoading(false)
+    form.resetFields()
+    setExpDate(null)
+    setStartDate(null)
+    setEditingPromotion(false)
   };
 
   const handleValuesChange = (changedValues, allValues) => {
@@ -58,16 +79,114 @@ function PromotionsManager() {
     });
   };
 
+  const itemsPerPage = {
+    pageSize: 5
+}
+
+const handleEditPromotion = (promotionID) => {
+    setSelectedPromotion(promotions.find((promo) => promo.id_promotion === promotionID))    
+    setEditingPromotion(true)
+
+    scroller.scrollTo('promotionsForm', {
+      duration: 500,
+      delay: 0,
+      smooth: 'easeInOutQuart',
+    });
+}
+
+const handleDeletePromotion = async(promotionID) =>{
+  setDeleting(true)
+  await delete_promotion(promotionID)
+  setDeleting(false)
+} 
+
+
+useEffect(()=> {
+  if (editingPromotion && selectedPromotion) {
+    const processedPromotions = processPromotions(selectedPromotion.id_product_promotion)
+    form.setFieldsValue({
+      promoName: selectedPromotion.name,
+      promoPrice: selectedPromotion.price,
+      groupProducts: processedPromotions,
+      setExpDate: selectedPromotion.end_date,
+      setStartDate: selectedPromotion.start_date,
+    })
+  }
+},[editingPromotion, selectedPromotion])
+
+const tablePromotion = [
+    {
+        title: "Promo",
+        key: "promo",
+        dataIndex: "name"
+    },
+    {
+        title: "Precio",
+        key: "price",
+        render: (_,record) => (
+            <p>
+                {parseFloat(record.price).toLocaleString("es-AR",{style:"currency", currency: "ARS"})}
+            </p>
+        )
+    },
+    {
+        title: "Fecha de comienzo",
+        key: "start_date",
+        render: (_,record) => (
+            <p>
+                {dayjs(record.start_date).format("DD-MM-YYYY")}
+            </p>
+        )
+    },
+    {
+        title: "Fecha de fín",
+        key: "end_date",
+        render: (_,record) => (
+            <p>
+                {dayjs(record.end_date).format("DD-MM-YYYY")}
+            </p>
+        )
+    },
+    {
+        key: "actions",
+        render: (_,record) => (
+            <>
+                <Button type="primary" onClick={()=> handleEditPromotion(record.id_promotion)}><Edit/></Button>
+                {" "}
+                <Popconfirm
+                onConfirm={()=>handleDeletePromotion(record.id_promotion)}
+                okText="Eliminar"
+                okType="danger"
+                cancelText="Cancelar"
+                okButtonProps={[
+                  {loading: deleting}
+                ]}
+                title="¿Está seguro que quiere eliminar esta promoción?"
+                description="Esta acción no se puede deshacer!"
+                >
+                  <Button type="primary" danger><DeleteIcon/></Button>
+                </Popconfirm>
+            </>
+        )
+    }
+]
+
   return (
     <>
       <AdminNavbar />
       <div className="admin__wrapper">
+        <div id="promotionsForm" ref={formRef}></div>
         <Row gutter={10} style={{ marginLeft: ".5rem" }}>
           <Col xs={24} sm={18} md={16} lg={12}>
-            <Card title="Selección de productos">
+            <Card title={editingPromotion ? <>
+              Editando una promoción { " " }
+              <Button type="primary" danger onClick={()=> setEditingPromotion(false)}>Cancelar editado</Button>
+              </> : "Crear una promoción"}>
+              <h4>Selección de productos</h4>
               <Form
                 name="promotionsProducts"
                 form={form}
+                
                 layout="vertical"
                 onFinish={handleFinish}
                 onValuesChange={handleValuesChange}
@@ -141,6 +260,7 @@ function PromotionsManager() {
 
                 <Form.Item
                   name="groupProducts"
+                  label={"¿Que productos desea agregar a la promo?"}
                   style={{ maxHeight: 350, overflowY: "auto" }}
                   rules={[
                     {
@@ -148,7 +268,8 @@ function PromotionsManager() {
                         if (value && value.length > 0) {
                           return Promise.resolve();
                         }
-                        return Promise.reject(new Error("Seleccione al menos dos productos"));
+                        message.error("Seleccione al menos un producto")
+                        return Promise.reject(new Error("Seleccione al menos un producto"));
                       },
                     },
                   ]}
@@ -164,14 +285,15 @@ function PromotionsManager() {
                   </Checkbox.Group>
                 </Form.Item>
                 <Form.Item>
-                  <Button type="primary" htmlType="submit">
+                  <Button type="primary" htmlType="submit" loading={loading}>
                     Crear Promoción
                   </Button>
                 </Form.Item>
               </Form>
             </Card>
           </Col>
-          <Col xs={24} sm={18} md={16} lg={12}>
+          
+          <Col  xs={24} sm={18} md={16} lg={12}>
             <Card title="Información de la promo">
               {selectedProducts.length > 0 ? (
                 <ul>
@@ -204,7 +326,17 @@ function PromotionsManager() {
                 {expDate ? `(${dayjs(expDate).diff(dayjs(), "day") + 1} días)` : ""}
               </h4>
             </Card>
+            <br />
+            <Card title="Lista de promos">
+              <Table
+              dataSource={promotions}
+              columns={tablePromotion}
+              pagination={itemsPerPage}
+              scroll={{x: 500}}
+              />
+            </Card>
           </Col>
+          
         </Row>
       </div>
       {contextHolder}
