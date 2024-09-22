e19d8894f61f005e89fb0360f2393e82f1b8787e
Carlos0550 <carlospelinski03@gmail.com>
2024-09-22 00:19:33 -0300

    se implemento la ruta de promociones

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css"
 import {Routes, Route} from "react-router-dom"
 import AdminPage from './pages/Administracion/pages/AdminPage'
 import ViewProducts from './pages/Administracion/pages/ViewProducts'
+import PromotionsManager from './pages/Administracion/pages/PromotionsManager'
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<AdminPage/>}/> 
         <Route path='/view_products/*' element={<ViewProducts/>}/>  
+        <Route path='/promotions' element={<PromotionsManager/>}/>
       </Routes>
     </>
   )
