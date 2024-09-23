53e7265194aaa879797b126acfe11ff675f5f1e2
Carlos0550 <carlospelinski03@gmail.com>
2024-09-23 19:56:54 -0300

    Se implemento un controlador de rutas

--- src/App.js
+++ src/App.js
@@ -1,19 +1,68 @@
-import React from 'react'
-import "./App.css"
-import {Routes, Route} from "react-router-dom"
-import AdminPage from './pages/Administracion/pages/AdminPage'
-import ViewProducts from './pages/Administracion/pages/ViewProducts'
-import PromotionsManager from './pages/Administracion/pages/PromotionsManager'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import AdminPage from './pages/Administracion/pages/AdminPage';
+import ViewProducts from './pages/Administracion/pages/ViewProducts';
+import PromotionsManager from './pages/Administracion/pages/PromotionsManager';
+import Login from './pages/Login/Login';
+import Settings from './pages/Administracion/pages/Settings';
+import Home from "./pages/Clientes/Pages/Inicio/Home";
+import { useAuthContext } from './AuthContext';
+
 function App() {
+  const { isAuthenticated, isAdmin } = useAuthContext();
+  console.log(isAdmin)
   return (
-    <>
-      <Routes>
-        <Route path='/' element={<AdminPage/>}/> 
-        <Route path='/view_products/*' element={<ViewProducts/>}/>  
-        <Route path='/promotions' element={<PromotionsManager/>}/>
-      </Routes>
-    </>
-  )
+    <Routes>
+      
+
+      {/* Ruta de login */}
+      <Route path="/login" element={<Login />} />
+
+      {/* Redirección si no está autenticado */}
+      <Route
+        path="/"
+        element={
+          isAuthenticated ? (
+            isAdmin ? <AdminPage /> : <Navigate to="/home" />
+          ) : (
+            <Navigate to="/login" />
+          )
+        }
+      />
+      <Route
+        path="/view_products"
+        element={
+          isAuthenticated ? (
+            isAdmin ? <ViewProducts /> : <Navigate to="/home" />
+          ) : (
+            <Navigate to="/login" />
+          )
+        }
+      />
+      <Route
+        path="/promotions"
+        element={
+          isAuthenticated ? (
+            isAdmin ? <PromotionsManager /> : <Navigate to="/home" />
+          ) : (
+            <Navigate to="/login" />
+          )
+        }
+      />
+      <Route
+        path="/settings"
+        element={
+          isAuthenticated ? (
+            isAdmin ? <Settings /> : <Navigate to="/home" />
+          ) : (
+            <Navigate to="/login" />
+          )
+        }
+      />
+
+      {/* Ruta pública */}
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
