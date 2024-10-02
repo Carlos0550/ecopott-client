1b4a13da2fb65765655947a9e1bfb07185865311
EcopottPruebas <129014343+Carlos0550@users.noreply.github.com>
2024-10-01 23:32:30 -0300

    .

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
-    apiBaseUrl: "macetas-brian-server.cyr-app.online"
+    apiBaseUrl: "https://macetas-brian-server.cyr-app.online"
     //"http://147.79.81.15:4000"
     //"http://localhost:4000"
 }
